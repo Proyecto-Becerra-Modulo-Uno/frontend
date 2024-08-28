d5da4b2b2ffd15d1fdde8f4853460fbc411c121c
VickyR-E <vrendonestrada@gmail.com>
2024-08-27 22:08:53 -0500

    Falta solucionar la ruta

---
+++ src/routes/routes.usuario.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { preguntas } from "../controllers/usuarios.controllers.js";
+
+
+
+const rutaPreguntas = Router();
+
+rutaPreguntas.get("/preguntas", preguntas)
+
+export default rutaPreguntas
