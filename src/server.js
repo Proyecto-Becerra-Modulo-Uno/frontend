6cf712769aa0ff9f0ffc104d2b16c82fef8e1706
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-08-28 18:04:53 -0500

    Corrección preguntas 2

--- src/server.js
+++ src/server.js
@@ -4,11 +4,8 @@ import ejs from "ejs";
 import path from "path";
 import { fileURLToPath } from "url";
 import ruta from "./routes/index.js";
-<<<<<<< HEAD
 import rutaPanel from "./routes/routes.panel.js";
 import rutaRecuperarClave from "./routes/routes.recuperarClave.js";
-=======
->>>>>>> 51eb4038dd29904ce27ce60601635325086b2fe8
 config();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -21,7 +18,6 @@ app.set("views", path.join(__dirname, "views"));
 
 app.set("port", process.env.PORT || 3000);
 
-<<<<<<< HEAD
 app.use("/", rutaPanel);
 app.use("/", rutaRecuperarClave);
 app.use("/", ruta);
@@ -29,12 +25,5 @@ app.use("/", ruta);
 // app.use("/", (req, res) => {
 //     res.render("views.error.ejs");
 // });
-=======
-app.use("/", ruta);
-
-app.use("/", (req, res) => {
-    res.render("views.error.ejs");
-});
->>>>>>> 51eb4038dd29904ce27ce60601635325086b2fe8
 
 export default app;
