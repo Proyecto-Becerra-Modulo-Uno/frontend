298d04c571b148adbd9c877c87d3dccd5acaf3c5
Santiago <santipahur@gmail.com>
2024-08-28 11:24:20 -0500

    recuperar contraseña parte 1 terminada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import Router from "express";
 import rutaPanel from "./routes.panel.js";
+import rutaRecuperarClave from "./routes.recuperarClave.js";
 
 const ruta = Router();
 
 ruta.use("/", rutaPanel);
+ruta.use("/", rutaRecuperarClave);
 
-export default ruta
+export default ruta;
