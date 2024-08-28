065b403f37c40d945b763da0104f96c18e6364ef
MarkCelius <richieskata@gmail.com>
2024-08-28 08:38:34 -0500

    menú actualizado

--- src/routes/routes.panel.js
+++ src/routes/routes.panel.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { panel } from "../controllers/panel.controllers.js";
+import { condicionesc, panel } from "../controllers/panel.controllers.js";
 
 const rutaPanel = Router();
 
 rutaPanel.get("/", panel)
+rutaPanel.get("/co", condicionesc)
+
 
 export default rutaPanel
