c4b8cb716a0397659b31b47cd07cd1010152653e
sneider1214t <sneidertrujillo16@gmail.com>
2024-08-28 10:58:44 -0500

    mi primer commit con lo pedido

--- src/routes/routes.panel.js
+++ src/routes/routes.panel.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { panel } from "../controllers/panel.controllers.js";
+import { panel, verificacionDoble1,verificacionDoble2 } from "../controllers/panel.controllers.js";
 
 const rutaPanel = Router();
 
 rutaPanel.get("/", panel)
 
-export default rutaPanel
+rutaPanel.get("/verificacionDoble1", verificacionDoble1)
+
+rutaPanel.get("/verificacionDoble2", verificacionDoble2)
+
+export default rutaPanel 
