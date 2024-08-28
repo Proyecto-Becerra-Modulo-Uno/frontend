fdd6063c9f112808fa46f76644e200a53801b943
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2024-08-28 14:55:53 -0500

    Merge padilla

--- src/controllers/panel.controllers.js
+++ src/controllers/panel.controllers.js
@@ -10,3 +10,10 @@ export const verificacionDoble1 = (req, res) => {
 export const verificacionDoble2 = (req, res) => {
     res.render("views.verificacionDoble2.ejs")
 }
+
+export const politicas = (req, res) => {
+    res.render("views.privacidad.ejs")
+}
+export const registro = (req, res) => {
+    res.render("views.registro.ejs")
+}
--- src/routes/routes.panel.js
+++ src/routes/routes.panel.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { panel, verificacionDoble1,verificacionDoble2 } from "../controllers/panel.controllers.js";
+import { panel, politicas, registro, verificacionDoble1,verificacionDoble2 } from "../controllers/panel.controllers.js";
 
 const rutaPanel = Router();
 
@@ -8,5 +8,8 @@ rutaPanel.get("/", panel)
 rutaPanel.get("/verificacionDoble1", verificacionDoble1)
 
 rutaPanel.get("/verificacionDoble2", verificacionDoble2)
+
+rutaPanel.get("/privacidad",politicas)
+rutaPanel.get("/registro",registro)
 
 export default rutaPanel 
