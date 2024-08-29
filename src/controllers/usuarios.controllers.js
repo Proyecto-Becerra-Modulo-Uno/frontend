677fe99a3654e23886d6f07337724577b7e236d0
Santiago <santipahur@gmail.com>
2024-08-29 09:44:39 -0500

    recuperar contraseña parte 2 y 3 terminada

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,23 +1,31 @@
 export const preguntas = (req, res) => {
-    res.render("views.preguntas.seguridad.ejs")
-}
+  res.render("views.preguntas.seguridad.ejs");
+};
 
 export const recuperarClave = (req, res) => {
   res.render("views.recuperarClave.ejs");
 };
 
+export const codigoRecuperacion = (req, res) => {
+  res.render("views.codigoRecuperacion.ejs");
+};
+
+export const nuevaClave = (req, res) => {
+  res.render("views.nuevaClave.ejs");
+};
+
 export const cambiarcontrasena = (req, res) => {
-    res.render("views.contrasena.ejs")
-}
+  res.render("views.contrasena.ejs");
+};
 
 export const recuperarusuario = (req, res) => {
-    res.render("views.recucorreo.ejs")
-}
+  res.render("views.recucorreo.ejs");
+};
 
 export const ingresarcodigo = (req, res) => {
-    res.render("views.codigo.ejs")
-}
+  res.render("views.codigo.ejs");
+};
 
 export const politicas = (req, res) => {
-    res.render("views.privacidad.ejs")
-}
+  res.render("views.privacidad.ejs");
+};
--- src/routes/routes.usuario.js
+++ src/routes/routes.usuario.js
@@ -1,13 +1,24 @@
 import { Router } from "express";
-import { cambiarcontrasena, ingresarcodigo, politicas, preguntas, recuperarClave, recuperarusuario } from "../controllers/usuarios.controllers.js";
+import {
+  cambiarcontrasena,
+  codigoRecuperacion,
+  ingresarcodigo,
+  nuevaClave,
+  politicas,
+  preguntas,
+  recuperarClave,
+  recuperarusuario,
+} from "../controllers/usuarios.controllers.js";
 
 const rutaUsuario = Router();
 
 rutaUsuario.get("/preguntas", preguntas);
 rutaUsuario.get("/recuperarClave", recuperarClave);
+rutaUsuario.get("/codigoRecuperacion", codigoRecuperacion);
+rutaUsuario.get("/nuevaClave", nuevaClave);
 rutaUsuario.get("/contrasena", cambiarcontrasena);
 rutaUsuario.get("/correo", recuperarusuario);
 rutaUsuario.get("/codigo", ingresarcodigo);
-rutaUsuario.get("/privacidad",politicas);
+rutaUsuario.get("/privacidad", politicas);
 
-export default rutaUsuario
+export default rutaUsuario;
