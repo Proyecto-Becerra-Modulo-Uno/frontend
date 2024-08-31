ba90abd2154e77c61d18e0a1101719fa7abe07a4
MarkCelius <richieskata@gmail.com>
2024-08-31 00:39:09 -0500

    dispositivos / popup

--- src/controllers/panel.controllers.js
+++ src/controllers/panel.controllers.js
@@ -6,3 +6,11 @@ export const panel = (req, res) => {
 export const condicionesc = (req, res) => {
     res.render("views.condicionesc.ejs")
 }
+
+export const dispositivos = (req, res) => {
+    res.render("views.dispositivos.ejs")
+}
+
+export const notipopup = (req, res) => {
+    res.render("views.popup.ejs")
+}
--- src/routes/routes.panel.js
+++ src/routes/routes.panel.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { condicionesc, panel } from "../controllers/panel.controllers.js";
+import { condicionesc, dispositivos, notipopup, panel } from "../controllers/panel.controllers.js";
 
 const rutaPanel = Router();
 
 rutaPanel.get("/", panel)
 rutaPanel.get("/co", condicionesc)
+rutaPanel.get("/dis", dispositivos)
+rutaPanel.get("/notipop", notipopup)
+
+
 
 
 export default rutaPanel
