aded23ffc26f832a43ab12d18084aa94503be258
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-09-01 16:44:33 -0500

    Commit para pull con equipo 1

--- src/routes/routes.admin.js
+++ src/routes/routes.admin.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { condicionesc, config, nuevousuario, panel, registro, registroIngreso, registroSesion, verificacionDoble1, verificacionDoble2, verRegistro } from "../controllers/admins.controllers.js";
+import { condicionesc, config, cuentasbloqueadas, nuevousuario, panel, registro, registroIngreso, registroSesion, verificacionDoble1, verificacionDoble2, verRegistro } from "../controllers/admins.controllers.js";
 const rutaAdmin = Router();
 
 rutaAdmin.get("/panel", panel)
@@ -12,6 +12,6 @@ rutaAdmin.get("/verificacionDoble2", verificacionDoble2)
 rutaAdmin.get("/registroIngreso", registroIngreso)
 rutaAdmin.get("/config", config)
 rutaAdmin.get("/sesiones", registroSesion)
-rutaAdmin.get("/bloqueos",cuentasbloqueadas)
+rutaAdmin.get("/bloqueos", cuentasbloqueadas)
 
 export default rutaAdmin
