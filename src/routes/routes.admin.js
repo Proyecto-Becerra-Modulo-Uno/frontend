a18ecfac007a324a8638c44370377b2c3330a5f0
Santiago <santipahur@gmail.com>
2024-09-04 11:39:38 -0500

    culpa de esneider intentando solucionar un problema que genero otro

--- src/routes/routes.admin.js
+++ src/routes/routes.admin.js
@@ -1,17 +1,29 @@
 import { Router } from "express";
-import { condicionesc, config, nuevousuario, panel, registro, registroIngreso, registroSesion, verificacionDoble1, verificacionDoble2, verRegistro } from "../controllers/admins.controllers.js";
+import {
+  condicionesc,
+  config,
+  cuentasbloqueadas,
+  nuevousuario,
+  panel,
+  registro,
+  registroIngreso,
+  registroSesion,
+  verificacionDoble1,
+  verificacionDoble2,
+  verRegistro,
+} from "../controllers/admins.controllers.js";
 const rutaAdmin = Router();
 
-rutaAdmin.get("/panel", panel)
-rutaAdmin.get("/nuevousuario", nuevousuario)
-rutaAdmin.get("/registro", registro)
-rutaAdmin.get("/registros", verRegistro)
-rutaAdmin.get("/co", condicionesc)
-rutaAdmin.get("/verificacionDoble1", verificacionDoble1)
-rutaAdmin.get("/verificacionDoble2", verificacionDoble2)
-rutaAdmin.get("/registroIngreso", registroIngreso)
-rutaAdmin.get("/config", config)
-rutaAdmin.get("/sesiones", registroSesion)
-rutaAdmin.get("/bloqueos",cuentasbloqueadas)
+rutaAdmin.get("/panel", panel);
+rutaAdmin.get("/nuevousuario", nuevousuario);
+rutaAdmin.get("/registro", registro);
+rutaAdmin.get("/registros", verRegistro);
+rutaAdmin.get("/co", condicionesc);
+rutaAdmin.get("/verificacionDoble1", verificacionDoble1);
+rutaAdmin.get("/verificacionDoble2", verificacionDoble2);
+rutaAdmin.get("/registroIngreso", registroIngreso);
+rutaAdmin.get("/config", config);
+rutaAdmin.get("/sesiones", registroSesion);
+rutaAdmin.get("/bloqueos", cuentasbloqueadas);
 
-export default rutaAdmin
+export default rutaAdmin;
