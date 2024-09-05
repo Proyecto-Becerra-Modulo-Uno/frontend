2e5fe27656fd8c44b2f02b2a87684602fff88f9d
sneider1214t <sneidertrujillo16@gmail.com>
2024-09-05 07:54:11 -0500

    plantilla

--- src/routes/routes.home.js
+++ src/routes/routes.home.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { login } from "../controllers/home.controllers.js";
+import { login, plantilla } from "../controllers/home.controllers.js";
 
 const rutaHome = Router();
 
 rutaHome.get("/", login)
-
+rutaHome.get("/plantilla", plantilla)
 export default rutaHome
