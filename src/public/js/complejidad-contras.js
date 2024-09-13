a551bf9b47facc7a3107fa85c3762703cf7e6218
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2024-09-13 15:01:40 -0500

    Definir intervalos y detectar y prevenir ataques

--- src/public/js/complejidad-contras.js
+++ src/public/js/complejidad-contras.js
@@ -4,18 +4,20 @@ const btn = document.getElementById("btnG").addEventListener("click", () => {
     const time = document.getElementById("medidaT").value;
     const longitud = parseInt(document.getElementById("cantC").value);
     const frecuencia = document.getElementById("frecuenciaCS").value;
+    const intervalo = parseInt(document.getElementById("cantInt").value);
     fetch(url + "/users/actualizar-politicas", {
         method: "PUT",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
             longitud: longitud,
             duracion: num + time,
-            frecuencia: frecuencia
+            frecuencia: frecuencia,
+            intervalo: intervalo
         })
     })
     .then(res => res.json())
     .then(data => {
-        console.log(data);
+        alert("Bien")
     })
     .catch(err => console.error(err))
 });
