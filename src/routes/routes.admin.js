d361e721878034df7804cb2e5e412854e4feae5d
keiner12345 <gzkeiner@gmail.com>
2024-09-16 18:19:46 -0500

    Configuracion de logs frontend listo

--- src/routes/routes.admin.js
+++ src/routes/routes.admin.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { condicionesc, complejidadPreguntas, cuentasbloqueadas, grupos, informeActividad, notipopup, nuevousuario, panel, registro, registroSesion, verificacionDoble1, verificacionDoble2, configuracion } from "../controllers/admins.controllers.js";
+import { comLogs } from "../controllers/usuarios.controllers.js";
 const rutaAdmin = Router();
 
 rutaAdmin.get("/panel", panel);
 rutaAdmin.get("/notipop", notipopup);//???
 rutaAdmin.get("/registro", registro);
 rutaAdmin.get("/co", condicionesc);
+rutaAdmin.get("/logs", comLogs);
 rutaAdmin.get("/verificacionDoble1", verificacionDoble1);
 rutaAdmin.get("/verificacionDoble2", verificacionDoble2);
 rutaAdmin.get("/config", configuracion);
