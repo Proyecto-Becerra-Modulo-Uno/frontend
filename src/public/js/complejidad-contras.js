643299a18f346bd903f9430cafb109fdbfae4fb3
marithzacasta <marithzacastano9.5@gmail.com>
2024-09-19 10:40:36 -0500

    Añadi alertas a las vistas hechas y ademas arregle un pco el diseño de condiciones

--- src/public/js/complejidad-contras.js
+++ src/public/js/complejidad-contras.js
@@ -1,38 +1,56 @@
 const url = localStorage.getItem('url');
 // Guardar Cambios
 const btn = document.getElementById("btnG").addEventListener("click", () => {
-    const longitud = document.getElementById("cantC").value;
-    const tiempoT = document.getElementById("tiempoT").value;
-    const frecuencia = document.getElementById("frecuenciaCS").value;
-    const intervalo = document.getElementById("intervaloCam").value;
-    const cantMin = document.getElementById("cantMin").value;
-    const cantMay = document.getElementById("cantMay").value;
-    const cantNum = document.getElementById("cantNum").value;
-    const cantEsp = document.getElementById("cantEsp").value;
-    
-    fetch(url + "/users/actualizar-politicas", {
-        method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            longitud: longitud,
-            duracion: tiempoT,
-            frecuencia: frecuencia,
-            intervalo: intervalo,
-            cant_min_minusculas: cantMin,
-            cant_min_mayusculas: cantMay,
-            cant_min_numeros: cantNum,
-            cant_min_caracteres_esp: cantEsp
-        })
+  const longitud = document.getElementById("cantC").value;
+  const tiempoT = document.getElementById("tiempoT").value;
+  const frecuencia = document.getElementById("frecuenciaCS").value;
+  const intervalo = document.getElementById("intervaloCam").value;
+  const cantMin = document.getElementById("cantMin").value;
+  const cantMay = document.getElementById("cantMay").value;
+  const cantNum = document.getElementById("cantNum").value;
+  const cantEsp = document.getElementById("cantEsp").value;
+
+  // Verificar que lo campos no esten vacios
+  if (!longitud || !tiempoT || !frecuencia || !intervalo || !cantMin || !cantMay || !cantNum || !cantEsp) {
+    Swal.fire({
+      icon: "warning",
+      title: "Campos vacios!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    return;
+  }
+
+  fetch(url + "/users/actualizar-politicas", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      longitud: longitud,
+      duracion: tiempoT,
+      frecuencia: frecuencia,
+      intervalo: intervalo,
+      cant_min_minusculas: cantMin,
+      cant_min_mayusculas: cantMay,
+      cant_min_numeros: cantNum,
+      cant_min_caracteres_esp: cantEsp
     })
+  })
     .then(res => res.json())
     .then(data => {
-        console.log(data);
+      console.log(data);
+      Swal.fire({
+        icon: "success",
+        title: "La informacion ha sido guardada",
+        showConfirmButton: false,
+        timer: 1500
+      });
+
     })
     .catch(err => console.error(err))
 });
 
-// Mostrar Datos
 
+// Mostrar Datos
 fetch(url + "/users/listarPoliticasYTerminos")
   .then((res) => res.json())
   .then((data) => {
@@ -40,15 +58,15 @@ fetch(url + "/users/listarPoliticasYTerminos")
       console.error("error al mostrar datos", data);
     } else {
 
-        document.getElementById("cantC").value = data.body[0].cant_caracteres;
-        document.getElementById("tiempoT").value = data.body[0].duracion_token;
-        document.getElementById("frecuenciaCS").value = data.body[0].frecuencia_copia_seguridad;
-        document.getElementById("intervaloCam").value = data.body[0].intervalos_cambio_contrasena;
+      document.getElementById("cantC").value = data.body[0].cant_caracteres;
+      document.getElementById("tiempoT").value = data.body[0].duracion_token;
+      document.getElementById("frecuenciaCS").value = data.body[0].frecuencia_copia_seguridad;
+      document.getElementById("intervaloCam").value = data.body[0].intervalos_cambio_contrasena;
 
-        document.getElementById("cantMin").value = data.body[0].cant_min_minusculas;
-        document.getElementById("cantMay").value = data.body[0].cant_min_mayusculas;
-        document.getElementById("cantNum").value = data.body[0].cant_min_numeros;
-        document.getElementById("cantEsp").value = data.body[0].cant_min_caracteres_esp;
+      document.getElementById("cantMin").value = data.body[0].cant_min_minusculas;
+      document.getElementById("cantMay").value = data.body[0].cant_min_mayusculas;
+      document.getElementById("cantNum").value = data.body[0].cant_min_numeros;
+      document.getElementById("cantEsp").value = data.body[0].cant_min_caracteres_esp;
 
     }
 
--- src/public/js/complejidad.preguntas.js
+++ src/public/js/complejidad.preguntas.js
@@ -13,39 +13,67 @@ const btn = document.getElementById("btnG").addEventListener("click", () => {
     const caracteresP = document.getElementById("caracteresP").value;
     const caracteresR = document.getElementById("caracteresR").value;
     const cantP = document.getElementById("cantP").value;
-    
+
+    // Verificar que lo campos no esten vacios
+    if (!caracteresP || !caracteresR || !cantP) {
+        Swal.fire({
+            icon: "warning",
+            title: "Campos vacios!",
+            showConfirmButton: false,
+            timer: 1500,
+        });
+        return;
+    }
+
     fetch(url + "/users/actualizarComplePreguntas", {
         method: "PUT",
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             caracteres_pregunta: caracteresP,
             caracteres_respuesta: caracteresR,
             cant_preguntas: cantP
         })
     })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => console.error(err))
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            Swal.fire({
+                icon: "success",
+                title: "La informacion ha sido guardada",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        })
+        .catch(err => console.error(err))
 });
 
+const btnC = document.getElementById("btnC").addEventListener("click", () => {
+    Swal.fire({
+        icon: "error",
+        title: "No se guardo la informacion",
+        showConfirmButton: false,
+        timer: 1500
+    });
+})
+
+
+
+
 // Mostrar Datos
-
 fetch(url + "/users/listarComplePreguntas")
-  .then((res) => res.json())
-  .then((data) => {
-    if (data.error) {
-      console.error("error al mostrar datos", data);
-    } else {
+    .then((res) => res.json())
+    .then((data) => {
+        if (data.error) {
+            console.error("error al mostrar datos", data);
+        } else {
 
-        document.getElementById("caracteresP").value = data.body[0].caracteres_pregunta;
-        document.getElementById("caracteresR").value = data.body[0].caracteres_respuesta;
-        document.getElementById("cantP").value = data.body[0].cant_preguntas;
+            document.getElementById("caracteresP").value = data.body[0].caracteres_pregunta;
+            document.getElementById("caracteresR").value = data.body[0].caracteres_respuesta;
+            document.getElementById("cantP").value = data.body[0].cant_preguntas;
 
-    }
+        }
 
-  })
-  .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 
 
